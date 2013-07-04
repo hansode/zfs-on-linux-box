c5716891390182e007562d58d466759ac02c2277
Masahito Yoshida <masahito@axsh.net>
2013-07-04 20:26:37 +0900

    first commit

---
+++ xexecscript.d/zfs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+
+declare chroot_dir=$1
+
+chroot $1 $SHELL <<'EOS'
+  yum localinstall -y --nogpgcheck http://archive.zfsonlinux.org/epel/zfs-release-1-2.el6.noarch.rpm
+  yum install -y zfs
+EOS
