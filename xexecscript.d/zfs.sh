d3c38e2d1f009b5f8adb24c272b90dd8c4041b50
Masahito Yoshida <masahito@axsh.net>
2013-07-05 00:34:27 +0900

    fix building zfs kernel module.

--- xexecscript.d/zfs.sh
+++ xexecscript.d/zfs.sh
@@ -10,4 +10,7 @@ declare chroot_dir=$1
 chroot $1 $SHELL <<'EOS'
   yum localinstall -y --nogpgcheck http://archive.zfsonlinux.org/epel/zfs-release-1-2.el6.noarch.rpm
   yum install -y zfs
+
+  chkconfig --add  zfs
+  chkconfig --list zfs
 EOS
