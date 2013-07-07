63876c773aea878cfea33625eee0b23baa465818
Masahito Yoshida <masahito@axsh.net>
2013-07-07 12:55:07 +0900

    Update zfs.sh

--- xexecscript.d/zfs.sh
+++ xexecscript.d/zfs.sh
@@ -11,6 +11,7 @@ chroot $1 $SHELL <<'EOS'
   yum localinstall -y --nogpgcheck http://archive.zfsonlinux.org/epel/zfs-release-1-2.el6.noarch.rpm
   yum install -y zfs
 
+  chkconfig --list zfs
   chkconfig --add  zfs
   chkconfig --list zfs
 EOS
